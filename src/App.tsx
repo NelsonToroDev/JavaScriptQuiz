78be88cd75f1fe769a9657e3d9acaf55d125ae40
Nelson Toro <nelson.toro.flores@gmail.com>
2024-03-25 16:12:29 -0400

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+function App () {
+  return (
+    <>
+      <h1>JavaScript Quiz</h1>
+    </>
+  )
+}
+
+export default App
