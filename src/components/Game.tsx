9a8a87cb706c16e7e13a138d8f7d26ee222af391
Nelson Toro <nelson.toro.flores@gmail.com>
2024-03-27 16:47:29 -0400

    Load answers to UI and mark correct or incorrect user answer

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,19 +1,61 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { Card, IconButton, Stack, Typography } from '@mui/material'
+import { Card, IconButton, List, ListItem, ListItemButton, ListItemText, Stack, Typography } from '@mui/material'
 import { useQuestionsStore } from '../store/questions'
 import { type Question } from '../types'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
-export const QuestionInfo = ({ info }: { info: Question }) => {
+// Function that will be create once that's why we keep it outside of the component
+const getBackgroundColor = (questionInfo: Question, index: number) => {
+  const { userSelectedAnswer, correctAnswer } = questionInfo
+
+  // no answer selected yet
+  if (userSelectedAnswer == null) return 'transparent'
+
+  // User has selected an answer and this index is neither userSelectected or correctAnswer
+  if (index !== correctAnswer && index !== userSelectedAnswer) return 'transparent'
+
+  // If it is the correct answer
+  if (index === correctAnswer) return 'green'
+
+  // If the selected answer is not right
+  if (index === userSelectedAnswer) return 'red'
+
+  // anything else
+  return 'transparent'
+}
+
+export const QuestionCard = ({ questionInfo }: { questionInfo: Question }) => {
+  const selectAnswer = useQuestionsStore(state => state.selectAnswer)
+
+  // This function return another function which will be the handle click
+  const createHandleClick = (answerIndex: number) => () => {
+    selectAnswer(questionInfo.id, answerIndex)
+  }
+
   return (
-    <Card variant='outlined' sx={{ bgcolor:'#222', p:2, textAlign: 'left'}}>
+    <Card variant='outlined' sx={{ bgcolor: '#222', p: 2, textAlign: 'left', marginTop: 4 }}>
       <Typography variant='h5'>
-        {info.question}
+        {questionInfo.question}
       </Typography>
       <SyntaxHighlighter language='javascript' style={gradientDark}>
-        {info.code}
+        {questionInfo.code}
       </SyntaxHighlighter>
+      <List sx={{ bgcolor: '#333' }} disablePadding>
+        {
+          questionInfo.answers.map((answer, index) => (
+            <ListItem key={index} disablePadding divider>
+              <ListItemButton
+                disabled={questionInfo.userSelectedAnswer != null}
+                onClick={createHandleClick(index)}
+                sx={{ backgroundColor: getBackgroundColor(questionInfo, index) }}
+              >
+                <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
+              </ListItemButton>
+            </ListItem>
+          ))
+        }
+      </List>
     </Card>
   )
 }
@@ -26,7 +68,7 @@ export const Game = () => {
 
   return (
     <>
-      <QuestionInfo info={questionInfo} />
+      <QuestionCard questionInfo={questionInfo} />
     </>
   )
 }
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,22 +1,45 @@
 import { create } from 'zustand'
 import { type Question } from '../types'
+import confetti from 'canvas-confetti'
 
 interface State {
   questions: Question[]
   currentQuestion: number
   fetchQuestions: (limit: number) => Promise<void>
+  selectAnswer: ((questionId: number, answerIndex: number) => void)
 }
 
 export const useQuestionsStore = create<State>((set, get) => {
   return {
     questions: [],
     currentQuestion: 0,
-    fetchQuestions: async (limit: number) => {
+    fetchQuestions: async (limit: number): Promise<void> => {
       const res = await fetch('http://localhost:5173/data.json')
       const json = await res.json()
       const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
 
       set({ questions })
+    },
+    selectAnswer: (questionId: number, answerIndex: number): void => {
+      const state = get()
+      const { questions } = state
+
+      const newQuestions = structuredClone(questions)
+      const questionIndex = newQuestions.findIndex(q => q.id === questionId)
+      const questionInfo = newQuestions[questionIndex]
+      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+
+      // update the state
+      newQuestions[questionIndex] = {
+        ...questionInfo,
+        userSelectedAnswer: answerIndex,
+        isCorrectUserAnswer
+      }
+
+      // confetti
+      if (isCorrectUserAnswer) confetti()
+
+      set({ questions: newQuestions })
     }
   }
 })
